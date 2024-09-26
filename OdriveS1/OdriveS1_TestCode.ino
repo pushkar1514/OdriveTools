304f887180bee13fa720fa153adceb50c394ce32
Akshay Wani <116380320+akshaywani1382004@users.noreply.github.com>
2024-09-26 17:25:44 +0530

    OdriveS1_TestCode.ino

--- OdriveS1/OdriveS1_TestCode.ino
+++ OdriveS1/OdriveS1_TestCode.ino
@@ -3,34 +3,17 @@
 
 // SoftwareSerial odrive_serial(8, 9); // RX (ODrive TX), TX (ODrive RX)
 
-ODriveArduino odrive(Serial3);
+ODriveArduino odrive(Serial5);       // ODrive connected on Serial 5
 
 int vel , axis = 0;
 int pos;
 
 
 void setup() {
-  Serial3.begin(115200);
-
+  Serial5.begin(115200);
   Serial.begin(115200);
   
-
-  Serial.println("Waiting for ODrive...");
-  while (odrive.getState() == AXIS_STATE_UNDEFINED) {
-    delay(10);
-  }
-  
-  Serial.print("DC voltage: ");
-  Serial.println(odrive.getParameterAsFloat("vbus_voltage"));
-  
-  Serial.println("Enabling closed loop control...");
-  while (odrive.getState() != AXIS_STATE_CLOSED_LOOP_CONTROL) {
-    odrive.clearErrors();
-    odrive.setState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-    delay(10);
-  }
-  
-  Serial.println("ODrive running!");
+  odrive.closedLoopState(); // Accessing Closed Loop
 }
 
 void loop() {
